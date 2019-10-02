51718e3aada765ada00b3d01c4d839c3e387dcbd
goandreus <andreshtml5@gmail.com>
2019-10-02 12:06:31 -0500

    runner

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/andreschavez/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andreschavez/Documents/Flutter Projects/GDG-DevFest-App"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/andreschavez/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.4"
+export "FLUTTER_BUILD_NUMBER=5"
